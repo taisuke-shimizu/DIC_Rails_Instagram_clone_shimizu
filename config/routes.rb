f286138e9344354c57a8694717d2875202348d17
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-02-09 14:39:30 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'user#new'
+  root to: 'users#new'
   resources :users, only: [:new, :create, :show, :edit]
+  resources :sessions, only: [:new, :create, :destroy]
 end
