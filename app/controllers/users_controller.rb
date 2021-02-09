b514c26b472eee349e2a1176039f964fb0b4dfee
taisuke shimizu <uj.l.ukbea.dy02@gmail.com>
2021-02-09 13:30:10 +0900

    ログイン,プロフィールページ実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'user#new'
+  resources :users, only: [:new, :create, :show, :edit]
 end
